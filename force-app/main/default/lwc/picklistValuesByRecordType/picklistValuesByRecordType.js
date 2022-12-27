875e180a22feceeed4827e78a43a39b25bca64e1
Ben Repak <34491091+BenRepak@users.noreply.github.com>
2022-12-27 14:48:05 -0700

    Update picklistValuesByRecordType.js

--- force-app/main/default/lwc/picklistValuesByRecordType/picklistValuesByRecordType.js
+++ force-app/main/default/lwc/picklistValuesByRecordType/picklistValuesByRecordType.js
@@ -4,7 +4,6 @@ import { FlowAttributeChangeEvent } from "lightning/flowSupport";
 
 export default class PicklistValuesByRecordType extends LightningElement {
   @track options;
-
   @track _value;
   @api fieldApiName;
   @api fieldLabel;
@@ -22,17 +21,24 @@ export default class PicklistValuesByRecordType extends LightningElement {
     if (data) {
       //console.log("data >>> " + data);
       this.options = data.picklistFieldValues[this.fieldApiName].values;
+      // console.log('data.picklistFieldValues[this.fieldApiName] ' + JSON.stringify(data.picklistFieldValues[this.fieldApiName]));
     } else if (error) {
       console.log("error =====> " + JSON.stringify(error));
     }
   }
 
   handleChange(event) {
-    console.log("changed6");
+    console.log("handleChange event");
+
     this._value = event.target.value;
     this.value = this._value;
+    // set to undefined to so validate works properly if switching from value to no value on required input
+    if(this._value == ""){
+      this._value = undefined;
+    }
+ 
 
-    //console.log("this._value >>> " + this._value);
+    console.log("this._value >>> " + this._value);
 
     const attributeChangeEvent = new FlowAttributeChangeEvent(
       "outputValue",
@@ -43,6 +49,9 @@ export default class PicklistValuesByRecordType extends LightningElement {
     // TODO https://developer.salesforce.com/docs/component-library/bundle/lightning-flow-support/documentation
     //https://salesforce.stackexchange.com/questions/282026/how-to-use-flowattributechangeevent-to-affect-component-visibility-in-flow
     //https://help.salesforce.com/articleView?id=flow_considerations_design_conditional_visibility.htm&type=0
+    // console.log("this.isRequired >>> " + this.isRequired);
+    // console.log("this.outputValue >>> " + this.outputValue);
+    // console.log("this.placeholder >>> " + this.placeholder);
   }
 
   //https://developer.salesforce.com/docs/component-library/documentation/en/lwc/use_build_for_flow_screens
@@ -53,6 +62,9 @@ export default class PicklistValuesByRecordType extends LightningElement {
     // console.log("this.isRequired >>> " + this.isRequired);
     // console.log("this.outputValue >>> " + this.outputValue);
     // console.log("this.placeholder >>> " + this.placeholder);
+    // console.log("this.placeholder >>> " + this.placeholder);
+
+
 
     if (this.isRequired === false) {
       return { isValid: true };
